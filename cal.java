c38f0622e1954ad18d2f3ed04013960e2cbdfccd
meghananvekar <megha.bhat3@gmail.com>
2026-03-23 11:06:52 +0530

    Update cal.java

--- cal.java
+++ cal.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class cal {
    public static void main(String[] args) {
-      double num1;
-      double num2;
+      double num1 = Double.parseDouble(args[0]);
+      double num2 = Double.parseDouble(args[1]);;
       double ans;
       char op;
       Scanner reader = new Scanner(System.in);
